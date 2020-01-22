d2242c08010bd7e599d283a5d9f161c3a2cd936e
semilunar <katyanikitina.me@yandex.ru>
2020-01-22 12:37:25 +0300

    planted replicas and answers through seed

---
+++ db/migrate/20200122092018_replicas_change_column_default.rb
@@ -0,0 +1,5 @@
+class ReplicasChangeColumnDefault < ActiveRecord::Migration[6.0]
+  def change
+    # change_column_default(:replicas, :type, :message)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-drop_db
-seed_data
-
-
-steps: [
+@steps = [
   {
     opening: true,
     replicas: [
       {
         position: 0,
-        phrase: 'Привет Реплика 1'
+        phrase: 'Шаг 1 Привет Реплика 1'
       },
       {
         position: 1,
-        phrase: 'Как дела Реплика 2'
+        phrase: 'Шаг 1 Как дела Реплика 2'
       },
       {
         position: 2,
-        phrase: 'Что делаешь Реплика 3'
+        phrase: 'Шаг 1 Что делаешь Реплика 3'
       }
     ],
     answers: [
       {
         position: 0,
-        phrase: 'ДА Ответ 1'
+        phrase: 'Шаг 1 ДА Ответ 1'
       },
       {
         position: 1,
-        phrase: 'НЕТ Ответ 2'
+        phrase: 'Шаг 1 НЕТ Ответ 2'
+      }
+    ]
+  },
+  {
+    opening: true,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 2 Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 2 Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 2 Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 2 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 2 НЕТ Ответ 2'
       }
     ]
   }
@@ -54,14 +69,32 @@ def seed_data
 end
 
 def create_steps
-  steps.each do |step|
-    s = Step.create!(opening: step.opening)
-    puts "Step created with opening #{ s.opening }"
+  @steps.each do |step|
+    s = Step.create!(opening: step[:opening])
+    puts "Step with opening #{ s[:opening] } just created"
+  end
 end
 
-def  create_replicas
-  # создать к этому степу реплики и ответы
+def create_replicas
+  @steps.each do |step|
+    replicas = step[:replicas]
+    replicas.each do |replica|
+      r = Replica.create!(position: replica[:position], phrase: replica[:phrase])
+      puts "Replica with position and phrase #{ r } was just created"
+    end
+  end
 end
 
-def  create_answers
+
+def create_answers
+  @steps.each do |step|
+    answers = step[:replicas]
+    answers.each do |answer|
+      a = Answer.create!(position: answer[:position], phrase: answer[:phrase])
+      puts "Answer with position and phrase #{ a } was just created"
+    end
+  end
 end
+
+drop_db
+seed_data
