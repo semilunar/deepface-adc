0eaa64c7e634dbca64b7173029b910ab33edf962
semilunar <katyanikitina.me@yandex.ru>
2020-01-28 19:26:53 +0300

    next steps created

--- app/models/next_step.rb
+++ app/models/next_step.rb
@@ -1,2 +1,4 @@
 class NextStep < ApplicationRecord
+  belongs_to :step
+  belongs_to :answer
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 @steps = [
   {
+    stage: 1,
     opening: true,
     replicas: [
       {
@@ -18,15 +19,18 @@
     answers: [
       {
         position: 0,
-        phrase: 'Шаг 1 ДА Ответ 1'
+        phrase: 'Шаг 1 ДА Ответ 1',
+        next_stage: 2
       },
       {
         position: 1,
-        phrase: 'Шаг 1 НЕТ Ответ 2'
+        phrase: 'Шаг 1 НЕТ Ответ 2',
+        next_stage: 3
       }
     ]
   },
   {
+    stage: 1,
     opening: true,
     replicas: [
       {
@@ -45,20 +49,301 @@
     answers: [
       {
         position: 0,
-        phrase: 'Шаг 2 ДА Ответ 1'
+        phrase: 'Шаг 2 ДА Ответ 1',
+        next_stage: 2
       },
       {
         position: 1,
-        phrase: 'Шаг 2 НЕТ Ответ 2'
+        phrase: 'Шаг 2 НЕТ Ответ 2',
+        next_stage: 3
+      }
+    ]
+  },
+  {
+    stage: 1,
+    opening: true,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 3 Привет Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 3 Как дела Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 3 Что делаешь Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 3 ДА Ответ 1',
+        next_stage: 2
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 3 НЕТ Ответ 2',
+        next_stage: 3
+      }
+    ]
+  },
+  {
+    stage: 1,
+    opening: true,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 4 Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 4 Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 4 Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 4 ДА Ответ 1',
+        next_stage: 2
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 4 НЕТ Ответ 2',
+        next_stage: 3
+      }
+    ]
+  },
+  {
+    stage: 2,
+    opening: false,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 5 Привет Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 5 Как дела Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 5 Что делаешь Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 5 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 5 НЕТ Ответ 2'
+      }
+    ]
+  },
+  {
+    stage: 2,
+    opening: false,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 6 Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 6 Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 6 Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 6 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 6 НЕТ Ответ 2'
+      }
+    ]
+  },
+  {
+    stage: 2,
+    opening: false,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 7 Привет Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 7 Как дела Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 7 Что делаешь Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 7 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 7 НЕТ Ответ 2'
+      }
+    ]
+  },
+  {
+    stage: 2,
+    opening: false,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 8 Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 8 Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 8 Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 8 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 8 НЕТ Ответ 2'
+      }
+    ]
+  },
+  {
+    stage: 3,
+    opening: false,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 9 Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 9 Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 9 Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 9 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 9 НЕТ Ответ 2'
+      }
+    ]
+  },
+  {
+    stage: 3,
+    opening: false,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 10 Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 10 Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 10 Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 10 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 10 НЕТ Ответ 2'
+      }
+    ]
+  },
+  {
+    stage: 3,
+    opening: false,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 11 Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 11 Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 11 Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 11 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 11 НЕТ Ответ 2'
+      }
+    ]
+  },
+  {
+    stage: 3,
+    opening: false,
+    replicas: [
+      {
+        position: 0,
+        phrase: 'Шаг 12 Реплика 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 12 Реплика 2'
+      },
+      {
+        position: 2,
+        phrase: 'Шаг 12 Реплика 3'
+      }
+    ],
+    answers: [
+      {
+        position: 0,
+        phrase: 'Шаг 12 ДА Ответ 1'
+      },
+      {
+        position: 1,
+        phrase: 'Шаг 12 НЕТ Ответ 2'
       }
     ]
   }
 ]
-
-hash = {
-  a: '123',
-  :b => '321',
-}
 
 def drop_db
  Rake::Task['db:drop'].invoke
@@ -68,18 +353,20 @@ end
 
 def seed_data
   create_steps
-  # create_replicas
-  # create_answers
+  create_next_steps
 end
+
 
 def create_steps
 
-  @steps.each do |step|
+  @steps.each_with_index do |step, index|
     s = Step.create!(opening: step[:opening])
-    puts "Step with opening #{ s[:opening] } just created"
+    puts "Step with opening #{ s.opening } just created"
+    @steps[index][:id] = s.id
+    puts @steps[index][:id]
 
     create_replicas(s, step[:replicas])
-    create_answers(s, step[:answers])
+    create_answers(index, s, step[:answers])
   end
 
 end
@@ -93,15 +380,28 @@ def create_replicas(step, replicas)
 
 end
 
-def create_answers(step, answers)
-
-  answers.each do |answer|
-    a = step.answers.create!(answer)
+def create_answers(index, step, answers)
+  answers.each_with_index do |answer, i|
+    a = step.answers.create!(position: answer[:position], phrase: answer[:phrase])
     puts "Answer with phrase '#{ a.phrase }' for step with id #{ a.step.id } just created"
+    @steps[index][:answers][i][:id] = a.id
   end
-
 end
 
+def create_next_steps
+  @steps.each do |step|
+    step[:answers].each do |answer|
+      if answer.has_key?(:next_stage)
+        @steps.each do |s|
+          if s[:stage] == answer[:next_stage]
+            ns = NextStep.create!(step_id: s[:id], answer_id: answer[:id])
+            puts "NextStep just created for step with id #{ ns.step.id } and answer with id #{ ns.answer.id }"
+          end
+        end
+      end
+    end
+  end
+end
 
 drop_db
 seed_data
